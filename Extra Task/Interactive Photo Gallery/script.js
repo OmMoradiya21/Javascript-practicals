48750c2179c79883a15ec07f7e3288f48050a092
OmMoradiya21 <ommoradiya21@gmail.com>
2026-07-28 22:00:19 +0530

    Add image view overlay and functionality to interactive photo gallery

--- Extra Task/Interactive Photo Gallery/script.js
+++ Extra Task/Interactive Photo Gallery/script.js
@@ -1,6 +1,19 @@
 const photoURL = document.getElementById("photoURL");
 const form = document.getElementById("form");
 const photoesContainer = document.getElementById("photoesContainer");
+const viewOverlay = document.getElementById("viewOverlay");
+const viewImage = document.getElementById("viewImage");
+const closeView = document.getElementById("closeView");
+
+const openView = (url) => {
+  viewImage.src = url;
+  viewOverlay.hidden = false;
+};
+
+const closeImageView = () => {
+  viewOverlay.hidden = true;
+  viewImage.src = "";
+};
 
 const deletePhoto = (id) => {
   const photo = document.getElementById(id);
@@ -17,10 +30,22 @@ form.addEventListener("submit", (e) => {
 
   photo.innerHTML = `
     <button type="button" onclick="deletePhoto('${id}')">Delete</button>
-    <a href=${URL}>
+    
     <img src=${URL}>
-    </a>
+    
     `;
   photoesContainer.prepend(photo);
   photoURL.value = "";
+});
+
+closeView.addEventListener("click", closeImageView);
+viewOverlay.addEventListener("click", (e) => {
+  if (e.target === viewOverlay) closeImageView();
+});
+
+photoesContainer.addEventListener("click", (e) => {
+  const img = e.target.closest("img");
+  if (!img) return;
+  e.preventDefault();
+  openView(img.src);
 });
